7b21d7279ecb7e93b4eb82ade26773feb0d55ca8
Alan Loh <alan.loh.up7@gmail.com>
2019-10-13 10:29:41 +0200

    Initialization of some modules

---
+++ nenupytf/other/const.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+__author__ = ['Alan Loh']
+__copyright__ = 'Copyright 2019, nenupytf'
+__credits__ = ['Alan Loh']
+__maintainer__ = 'Alan Loh'
+__email__ = 'alan.loh@obspm.fr'
+__status__ = 'Production'
+__all__ = [
+    'max_bsn'
+    ]
+
+
+max_bsn = 200e6 / 1024.
--- setup.py
+++ setup.py
@@ -5,16 +5,22 @@ import nenupytf
 meta_file = open('nenupytf/metadata.py').read()
 metadata  = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", meta_file))
 
-setup(name       = 'nenupytf',
-    packages     = find_packages(),
-    include_package_data=True,
-    install_requires=['numpy', 'astropy'],
-    python_requires='>=3.5',
-    scripts      = [],
-    version      = nenupytf.__version__,
-    description  = 'NenuFAR Python package',
-    url          = 'https://github.com/AlanLoh/nenupy-tf.git',
-    author       = metadata['author'],
+setup(
+    name = 'nenupytf',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ['numpy', 'astropy'],
+    python_requires = '>=3.5',
+    scripts = [],
+    version = nenupytf.__version__,
+    description = 'NenuFAR Python package',
+    url = 'https://github.com/AlanLoh/nenupy-tf.git',
+    author = metadata['author'],
     author_email = metadata['email'],
-    license      = 'MIT',
-    zip_safe     = False)
+    license = 'MIT',
+    zip_safe = False
+    )
+
+
+# Install on nancep
+# /usr/local/bin/pip3.5 install nenupytf --install-option=--prefix=/cep/lofar/nenupytf --upgrade
