f20475bb3f9dfe3a7a29cc9db7dee7e01a0a84e6
Alan Loh <alan.loh.up7@gmail.com>
2019-12-02 14:50:52 +0100

    dispersion delay function added

---
+++ nenupytf/process/astro.py
@@ -0,0 +1,55 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+"""
+    ****
+    Astro
+    ****
+
+    Test de docstring
+"""
+
+
+__author__ = ['Alan Loh']
+__copyright__ = 'Copyright 2019, nenupytf'
+__credits__ = ['Alan Loh']
+__maintainer__ = 'Alan Loh'
+__email__ = 'alan.loh@obspm.fr'
+__status__ = 'Production'
+__all__ = [
+    'dispersion_delay'
+    ]
+
+
+import astropy.units as u
+
+
+# ============================================================= #
+# ---------------------- dispersion_delay --------------------- #
+# ============================================================= #
+def dispersion_delay(f1, f2, dm):
+    """ Compute the delay between two frequencies of a dispersed
+        astrophysical signal with a known dispersion measure
+        :attr:`dm`.
+
+        :param f1:
+            First frequency in MHz
+        :type f1: float
+        :param f2:
+            Second frequency in MHz
+        :type f2: float
+        :param dm:
+            Dispersion measure in pc/cm^3
+        :type dm: float
+
+        :returns: delay in seconds
+        :rtype: `astropy.units.quantity.Quantity`
+    """
+    dm *= u.parsec / u.cm**3
+    f1 *= u.MHz
+    f2 *= u.MHz
+    k = 2.410e-4 * u.MHz**-2 * u.cm**-3 * u.parsec * u.s**-1
+    df2 = (f1**-2 - f2**-2)
+    delay = dm / k * df2
+    return delay
